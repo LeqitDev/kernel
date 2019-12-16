010473a2a6db2e7e50f26cfef72d9f9cd0d9e35c
Felix (xq) Queißner <git@mq32.de>
2019-12-16 18:40:12 +0100

    Adds zig build script.

---
+++ build.zig
@@ -0,0 +1,39 @@
+const std = @import("std");
+const builtin = @import("builtin");
+const Builder = @import("std").build.Builder;
+
+const cflags = [_][]const u8{
+    "-Wall",
+    "-Wextra",
+    "-fno-stack-protector",
+};
+
+pub fn build(b: *Builder) void {
+    const mode = b.standardReleaseOptions();
+    const kernel = b.addExecutable("kernel", null);
+
+    kernel.addCSourceFile("init.c", cflags);
+    kernel.addCSourceFile("console.c", cflags);
+    kernel.addAssemblyFile("start.S");
+
+    kernel.setBuildMode(mode);
+    kernel.setLinkerScriptPath("kernel.ld");
+    kernel.setTheTarget(std.build.Target{
+        .Cross = std.build.CrossTarget{
+            .arch = builtin.Arch.i386,
+            .os = builtin.Os.freestanding,
+            .abi = builtin.Abi.eabi,
+        },
+    });
+
+    kernel.install();
+
+    const qemu_step = b.addSystemCommand([_][]const u8{
+        "qemu-system-i386",
+        "-kernel",
+        "zig-cache/bin/kernel",
+    });
+
+    const run_step = b.step("run", "Run the OS with QEMU");
+    run_step.dependOn(&qemu_step.step);
+}
