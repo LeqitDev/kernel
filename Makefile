bbd1ad4b3bf1eec6755d6930024b00f25d166c0d
Markus Hamacher <markus.hamacher16@gmail.com>
2020-02-01 20:41:40 +0100

    lgcc fix

---
+++ Makefile
@@ -0,0 +1,9 @@
+all: kernel
+
+kernel:
+	$(MAKE) -C kernel
+
+clean:
+	$(MAKE) -C kernel clean
+
+.PHONY: all kernel
--- update.bat
+++ update.bat
@@ -1,7 +1,7 @@
 @echo off
 
-bash -c "/mnt/e/Downloads/zig-linux/zig-linux-x86_64-0.5.0+2c7a2aefb/zig build"
+bash -c "make"
 
-cd zig-cache/bin/
-qemu-system-x86_64.exe -kernel kernel
-cd ../..
+qemu-system-x86_64.exe -kernel kernel/kernel
+
+bash -c "make clean"
